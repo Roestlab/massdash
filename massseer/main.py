d0a831f3c8ec40b09c9d11bded8afa40ba6128f8
singjc <justincsing@gmail.com>
2023-10-23 02:06:24 -0400

    [ADD] main CLI interface

---
+++ massseer/main.py
@@ -0,0 +1,23 @@
+import click
+import sys
+import os
+from streamlit.web import cli as stcli
+
+@click.group(chain=True)
+@click.version_option()
+def cli():
+    """
+    MassSeer: Streamlined DIA-MS visualization, analysis, optimization and rapid prototyping. 
+    """
+
+# GUI for MassSeer.
+@cli.command()
+def gui():
+    """
+    GUI for MassSeer.
+    """
+    click.echo("Starting MassSeer GUI...")
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, 'gui.py')
+    sys.argv = ["streamlit", "run", filename]
+    sys.exit(stcli.main())
