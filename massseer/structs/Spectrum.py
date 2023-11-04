041e7af25ac4e29571917ae65f3d85fb6d55477c
singjc <justincsing@gmail.com>
2023-11-04 16:06:01 -0400

    [ADD] FeatureMap class and Mobilogram and Spectrum classes

---
+++ massseer/structs/Spectrum.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+class Spectrum:
+    ''' 
+    This is a single spectrum object. Holds the data and metadata of a spectrum
+    '''
+    def __init__(self, mz, intensity, label):
+        self.intensity = np.array(intensity)
+        self.mz = np.array(mz)
+        self.label = label
+
+    def __str__(self):
+        return f"{'-'*8} Spectrum {'-'*8}\nlabel: {self.label}\nlength of spectrum: {len(self.mz)}"
