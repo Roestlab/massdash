645c70033c9811cec69c31a953a836ab2ec6645d
singjc <justincsing@gmail.com>
2023-11-04 02:11:44 -0400

    [ADD] docs and added adders to Protein and Peptide instead of passing as arg

--- massseer/structs/Precursor.py
+++ massseer/structs/Precursor.py
@@ -1,7 +1,16 @@
 class Precursor:
-    '''
-    Class to represent a precursor
-    '''
+    """
+    A class representing a precursor ion in mass spectrometry.
+
+    Attributes:
+    -----------
+    mz : float
+        The m/z value of the precursor ion.
+    charge : int
+        The charge state of the precursor ion.
+    """
+
     def __init__(self, mz: float, charge: int):
-        self.mz = mz
+        self.mz = mz            
         self.charge = charge
+
--- massseer/structs/Protein.py
+++ massseer/structs/Protein.py
@@ -1,11 +1,29 @@
 from massseer.structs.Peptide import Peptide
-from typing import List
 
 class Protein:
     '''
-    Class to represent a protein
-    '''
+    Class to represent a protein.
 
-    def __init__(self, accession: str, peptides: List[Peptide]):
+    Attributes:
+    -----------
+    accession : str
+        The protein's accession number.
+    peptides : list
+        A list of Peptide objects representing the protein's peptides.
+    '''
+    def __init__(self, accession: str):
         self.accession = accession
-        self.peptides = peptides
+        self.peptides = []
+
+    def add_peptide(self, peptide: Peptide) -> None:
+            """
+            Adds a peptide to the protein's list of peptides.
+
+            Args:
+                peptide (Peptide): The peptide to add.
+
+            Returns:
+                None
+            """
+            self.peptides.append(peptide)
+
