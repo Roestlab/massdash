fab6eab8869be1929f70e99ba2f06c6adc51f46a
Joshua Charkow <joshuacharkow@gmail.com>
2023-11-13 16:13:03 -0500

    update transitionFeature
    add method to convert list of transitionFeatures to a dataframe

--- massseer/structs/TransitionFeature.py
+++ massseer/structs/TransitionFeature.py
@@ -1,11 +1,32 @@
 from massseer.structs.GenericFeature import GenericFeature
-from typing import Optional, Tuple
+from typing import Optional, Tuple, List
+import pandas as pd
+import numpy as np
 
 class TransitionFeature(GenericFeature):
-    def __init__(self, leftWidth: Optional(float) = None, rightWidth: Optional[float] = None, areaIntensity: Optional[float] = None, peakApex: Optional[float] = None, apexIntensity: Optional[float] = None):
-        super().__init__(leftWidth, rightWidth, areaIntensity)
+    def __init__(self, 
+                 leftBoundary: Optional[float] = None, 
+                 rightBoundary: Optional[float] = None, 
+                 areaIntensity: Optional[float] = None, 
+                 peakApex: Optional[float] = None,
+                 apexIntensity: Optional[float] = None):
+        super().__init__(leftBoundary, rightBoundary, areaIntensity)
         self.peakApex = peakApex
         self.apexIntensity = apexIntensity
 
     def getBoundaries(self) -> Tuple[float, float]:
         return super().getBoundaries()
+
+    @staticmethod
+    def toPandasDf(transitionFeatureLst: List["TransitionFeature"]) -> pd.DataFrame:
+        '''
+        Convert a list of TransitionFeature objects to a pandas dataframe
+        '''
+        leftBoundaries = [i.leftBoundary for i in transitionFeatureLst ]
+        rightBoundaries = [i.rightBoundary for i in transitionFeatureLst ]
+        areaIntensities = [i.areaIntensity for i in transitionFeatureLst ]
+        peakApexes = [i.peakApex for i in transitionFeatureLst ]
+        apexIntensities = [i.apexIntensity for i in transitionFeatureLst ]
+
+        return pd.DataFrame(np.column_stack([leftBoundaries, rightBoundaries, areaIntensities, peakApexes, apexIntensities]), 
+                            columns=['leftBoundary', 'rightBoundary', 'areaIntensity', 'peakApex', 'apexIntensity'])
