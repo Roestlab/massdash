1f622297ca7db5b95d7cd315a0357cacae6fae23
singjc <justincsing@gmail.com>
2023-11-14 21:20:07 -0500

    [REFACTOR] UI into separate module classes

---
+++ massseer/ui/ChromatogramPlotUISettings.py
@@ -0,0 +1,90 @@
+import streamlit as st
+
+class ChromatogramPlotUISettings:
+    """
+    A class for managing the settings for a chromatogram plot.
+
+    Attributes:
+        include_ms1 (bool): Whether to include MS1 traces in the plot.
+        include_ms2 (bool): Whether to include MS2 traces in the plot.
+        num_plot_columns (int): The number of columns to display the plots in.
+        set_x_range (bool): Whether to set a custom x-axis range for the plot.
+        set_y_range (bool): Whether to set a custom y-axis range for the plot.
+        do_smoothing (str): The type of smoothing to apply to the chromatograms.
+        smoothing_dict (dict): A dictionary of additional settings for the smoothing.
+
+    Methods:
+        create_sidebar(): Creates a sidebar in Streamlit for adjusting the plot settings.
+        get_settings(): Returns a dictionary of the current plot settings.
+    """
+    def __init__(self, massseer_gui):
+        self.massseer_gui = massseer_gui
+
+        self.include_ms1 = True
+        self.include_ms2 = True
+        self.num_plot_columns = 2
+        self.set_x_range = False
+        self.set_y_range = False
+        self.do_smoothing = 'none'
+        self.smoothing_dict = {}
+        self.scale_intensity = False
+
+    def create_sidebar(self):
+        """
+        Creates a sidebar in Streamlit for adjusting the plot settings.
+
+        Returns:
+            self (ChromatogramPlotSettings): The current instance of the ChromatogramPlotSettings class.
+        """
+        st.sidebar.title("Plotting Settings")
+        # Add checkboxes in the sidebar to include MS1 and/or MS2 traces
+        col1, col2 = st.sidebar.columns(2)
+        with col1:
+            self.include_ms1 = st.checkbox("Show MS1 Traces", value=self.include_ms1)
+        with col2:
+            self.include_ms2 = st.checkbox("Show MS2 Traces", value=self.include_ms2)
+
+        with st.sidebar.expander("Advanced Settings"):
+            # Display plots in N columns
+            self.num_plot_columns = st.number_input("Number of Columns", min_value=1, value=self.num_plot_columns)
+
+            # Add Checkboxes for setting x-range and y-range
+            col1, col2 = st.columns(2)
+            with col1:
+                self.set_x_range = st.checkbox("Link x-range", value=self.set_x_range)
+            with col2:
+                self.set_y_range = st.checkbox("Link y-range", value=self.set_y_range)
+
+            # Perform Smoothing of the chromatograms
+            self.do_smoothing = st.selectbox("Smoothing", ['sgolay', 'none'])
+
+            self.smoothing_dict['type'] = self.do_smoothing
+            if self.do_smoothing == 'sgolay':
+                # Create two columns for side-by-side widgets
+                col1, col2 = st.columns(2)
+
+                # Add widget for sgolay_polynomial_order in the first column
+                self.smoothing_dict['sgolay_polynomial_order'] = col1.number_input("Polynomial Order", min_value=1, max_value=10, value=3, step=1)
+
+                # Add widget for sgolay_frame_length in the second column
+                self.smoothing_dict['sgolay_frame_length'] = col2.number_input("Frame Length", min_value=1, max_value=50, value=11, step=1)
+            
+            self.scale_intensity = st.checkbox("Scale Intensity", value=False, key='plotting_settings_scale_intensity')
+        return self  # Return self for method chaining
+
+    def get_settings(self):
+        """
+        Returns a dictionary of the current plot settings.
+
+        Returns:
+            settings (dict): A dictionary of the current plot settings.
+        """
+        return {
+            "include_ms1": self.include_ms1,
+            "include_ms2": self.include_ms2,
+            "num_plot_columns": self.num_plot_columns,
+            "set_x_range": self.set_x_range,
+            "set_y_range": self.set_y_range,
+            "do_smoothing": self.do_smoothing,
+            "smoothing_dict": self.smoothing_dict,
+        }
---
+++ massseer/ui/MassSeerGUI.py
@@ -0,0 +1,79 @@
+import streamlit as st
+
+from massseer.ui.FileInputUISettings import FileInputUISettings
+from massseer.ui.ChromatogramPlotUISettings import ChromatogramPlotUISettings
+from massseer.ui.PeakPickingUISettings import PeakPickingUISettings
+from massseer.ui.ConcensusChromatogramUISettings import ConcensusChromatogramUISettings
+
+
+class MassSeerGUI:
+    def __init__(self):
+        self.welcome_container = st.empty()
+
+    def clicked(self, button):
+        """
+        Updates the session state to indicate that a button has been clicked.
+
+        Args:
+            button (str): The name of the button that was clicked.
+
+        Returns:
+            None
+        """
+        st.session_state.clicked[button] = True
+
+    def show_welcome_message(self):
+        """
+        Displays a welcome message and input fields for OpenSwath and DIA-NN workflows.
+
+        Returns:
+        welcome_container (streamlit.container): A container for the welcome message.
+        load_toy_dataset (streamlit.button): A button to load the OpenSwath example dataset.
+        osw_file_path (streamlit.text_input): A text input field for the OpenSwath file path.
+        sqmass_file_path_input (streamlit.text_input): A text input field for the sqMass file path.
+        """
+        # Add a welcome message
+        # welcome_container = st.empty()
+        with self.welcome_container:
+            with st.container():
+                st.title("Welcome to MassSeer!")
+                st.write("MassSeer is a powerful platform designed for researchers and analysts in the field of mass spectrometry.")
+                st.write("It enables the visualization of chromatograms, algorithm testing, and parameter optimization, crucial for data analysis and experimental design.")
+                st.write("This tool is an indispensable asset for researchers and laboratories working with DIA (Data-Independent Acquisition) data.")
+
+                # Tabs for different data workflows
+                self.tab1, self.tab2 = st.tabs(["Extracted Ion Chromatograms", "Raw Mass Spectrometry Data"])
+                with self.tab1:
+
+                    st.write("This workflow is designed for post-extracted ion chromatogram data. For example sqMass files generated from an OpenSwathWorkflow experiment.")
+
+                    st.subheader("OpenSwath")
+
+                    self.load_toy_dataset = st.button('Load OpenSwath Example', on_click=self.clicked , args=['load_toy_dataset'])
+
+                    st.title("Input OSW file")
+                    self.osw_file_path = st.text_input("Enter file path", "*.osw", key='osw_file_path_tmp')
+
+                    st.title("Input sqMass file")
+                    self.sqmass_file_path_input = st.text_input("Enter file path", "*.sqMass", key='sqmass_file_path_input_tmp')
+
+        return self
+
+    def show_file_input_settings(self, feature_file_path=None, xic_file_path=None):
+        self.file_input_settings = FileInputUISettings(self)
+        self.file_input_settings.create_sidebar(feature_file_path, xic_file_path)
+        self.file_input_settings.get_sqmass_files()
+        st.divider()
+        return self
+
+    def show_chromatogram_plot_settings(self):
+        self.chromatogram_plot_settings = ChromatogramPlotUISettings(self)
+        self.chromatogram_plot_settings.create_sidebar()
+        return self
+
+    def show_algorithm_settings(self):
+        self.peak_picking_settings = PeakPickingUISettings(self)
+        self.peak_picking_settings.create_ui(self.chromatogram_plot_settings)
+        self.concensus_chromatogram_settings = ConcensusChromatogramUISettings(self)
+        self.concensus_chromatogram_settings.create_ui(self.chromatogram_plot_settings)
+        return self
