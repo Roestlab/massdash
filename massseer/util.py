2913127fd9d537d050859a3eff44828f29208d99
singjc <justincsing@gmail.com>
2023-11-21 13:07:25 -0500

    [CLEAN] main util file

--- massseer/util.py
+++ massseer/util.py
@@ -1,52 +1,107 @@
-import os
 import sys
-import glob
-import base64
-import click
-import ast
 
 # Logging and performance modules
 from functools import wraps
 import contextlib
 from time import time
-from timeit import default_timer 
-from datetime import datetime, timedelta
+from timeit import default_timer
+from datetime import timedelta
 import logging
 from logging.handlers import TimedRotatingFileHandler
 
-# Type hinting
-from typing import List, Tuple
 
-import pyopenms as po
 
-# Common environment variables
-PROJECT_FOLDER = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
-WORKING_FOLDER = os.getcwd()
-MASSPEC_FILE_FORMATS = ["mzML", "mzXML", "raw", "wiff", "d"]
+#######################################
+## Logging Utils
 
 # Logging
 FORMATTER = logging.Formatter("[%(asctime)s] %(name)s - %(levelname)s - %(message)s")
 LOG_FILE = "MassSeer.log"
 
-# Alorithms
-PEAK_PICKING_ALGORITHMS = ["OSW-PyProphet","PeakPickerMRM"]
+def get_console_handler():
+    """
+    Returns a console handler for logging messages to the console.
 
-# Common methods
-def get_data_folder():
-    return os.path.join(PROJECT_FOLDER, "data")
+    Returns:
+        logging.StreamHandler: Console handler with the specified formatter.
+    """
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(FORMATTER)
+    return console_handler
 
-def get_input_folder():
-    return os.path.join(get_data_folder(), "input")
+def get_file_handler():
+    """
+    Returns a file handler for logging.
 
-def get_output_folder():
-    return os.path.join(get_data_folder(), "output")
+    Returns:
+        file_handler (TimedRotatingFileHandler): File handler for logging.
+    """
+    file_handler = TimedRotatingFileHandler(LOG_FILE, when='midnight')
+    file_handler.setFormatter(FORMATTER)
+    return file_handler
 
-# @st.cache_data()
-def get_base64_of_bin_file(png_file):
-    """Convert a binary file to the corresponding base64 representation"""
-    with open(png_file, "rb") as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
+def get_logger(logger_name):
+    """
+    Get a logger with the specified name.
+
+    Args:
+        logger_name (str): The name of the logger.
+
+    Returns:
+        logging.Logger: The logger object.
+
+    """
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG) # better to have too much log than not enough
+    logger.addHandler(get_console_handler())
+    logger.addHandler(get_file_handler())
+    # with this pattern, it's rarely necessary to propagate the error up to parent
+    logger.propagate = False
+    return logger
+
+# Call first instance of logger
+LOGGER = get_logger('MassSeer')
+
+@contextlib.contextmanager
+def code_block_timer(ident, log_type):
+    """
+    A context manager that measures the execution time of a code block.
+
+    Args:
+        ident (str): Identifier for the code block.
+        log_type (function): Logging function to output the elapsed time.
+
+    Yields:
+        None
+
+    Example:
+        with code_block_timer("my_code_block", logger.info):
+            # Code block to measure execution time
+            ...
+    """
+    tstart = time()
+    yield
+    elapsed = time() - tstart
+    log_type("{0}: Elapsed {1} ms".format(ident, elapsed))
+
+@contextlib.contextmanager
+def time_block():
+    """
+    A context manager that measures the time it takes to execute a block of code.
+
+    Usage:
+    with time_block() as elapsed_time:
+        # code block to be timed
+
+    Returns:
+    A timedelta object representing the elapsed time.
+    """
+    start = end = default_timer()
+    yield lambda: timedelta(seconds=end - start)
+    end = default_timer()
+
+#######################################
+## Data Handling Utils
 
 def copy_attributes(source_instance, destination_instance):
     """
@@ -63,28 +118,6 @@ def copy_attributes(source_instance, destination_instance):
     for attr_name, attr_value in vars(source_instance).items():
         # Set the attribute in the destination instance
         setattr(destination_instance, attr_name, attr_value)
-
-class conditional_decorator(object):
-    """
-    A decorator that applies another decorator to a function only if a condition is met.
-    See: https://gist.github.com/T1T4N/22ca7b0764cefe917b2b3a6bb056364c
-
-    Args:
-        dec (function): The decorator to apply.
-        condition (bool): The condition that must be met for the decorator to be applied.
-
-    Returns:
-        function: The decorated function, or the original function if the condition is not met.
-    """
-    def __init__(self, dec, condition):
-        self.decorator = dec
-        self.condition = condition
-
-    def __call__(self, func):
-        if not self.condition:
-            # Return the function unchanged, not decorated.
-            return func
-        return self.decorator(func)
     
 def check_streamlit():
     """
@@ -150,39 +183,42 @@ def check_sqlite_column_in_table(con, table, column):
 
     return(column_present)
 
-def setCompressionOptions(opt):
-    """
-    Adds suitable compression options for an object of type
-    pyopenms.PeakFileOptions
-        - compresses mass / time arrays with numpress linear
-        - compresses intensity with slof (log integer)
-        - compresses ion mobility with slof (log integer)
-    """
-    cfg = po.NumpressConfig()
-    cfg.estimate_fixed_point = True
-    cfg.numpressErrorTolerance = -1.0 # skip check, faster
-    cfg.setCompression(b"linear");
-    cfg.linear_fp_mass_acc = -1; # set the desired RT accuracy in seconds
-    opt.setNumpressConfigurationMassTime(cfg)
-    cfg = po.NumpressConfig()
-    cfg.estimate_fixed_point = True
-    cfg.numpressErrorTolerance = -1.0 # skip check, faster
-    cfg.setCompression(b"slof");
-    opt.setNumpressConfigurationIntensity(cfg)
-    opt.setCompression(True) # zlib compression
+#######################################
+## Decorators
 
-    # Now also try to compress float data arrays (this is not enabled in all
-    # versions of pyOpenMS).
-    try:
-        cfg = po.NumpressConfig()
-        cfg.estimate_fixed_point = True
-        cfg.numpressErrorTolerance = -1.0 # skip check, faster
-        cfg.setCompression(b"slof");
-        opt.setNumpressConfigurationFloatDataArray(cfg)
-    except Exception:
-        pass
+class conditional_decorator(object):
+    """
+    A decorator that applies another decorator to a function only if a condition is met.
+    See: https://gist.github.com/T1T4N/22ca7b0764cefe917b2b3a6bb056364c
+
+    Args:
+        dec (function): The decorator to apply.
+        condition (bool): The condition that must be met for the decorator to be applied.
+
+    Returns:
+        function: The decorated function, or the original function if the condition is not met.
+    """
+    def __init__(self, dec, condition):
+        self.decorator = dec
+        self.condition = condition
+
+    def __call__(self, func):
+        if not self.condition:
+            # Return the function unchanged, not decorated.
+            return func
+        return self.decorator(func)
 
 def method_timer(f):
+    """
+    Decorator that measures the execution time of a method.
+
+    Args:
+        f (function): The method to be timed.
+
+    Returns:
+        function: The wrapped method.
+
+    """
     @wraps(f)
     def wrap(*args, **kw):
         ts = time()
@@ -193,75 +229,3 @@ def method_timer(f):
         return result
     return wrap
 
-
-@contextlib.contextmanager
-def code_block_timer(ident, log_type):
-    tstart = time()
-    yield
-    elapsed = time() - tstart
-    log_type("{0}: Elapsed {1} ms".format(ident, elapsed))
-
-@contextlib.contextmanager
-def time_block():
-    """
-    A context manager that measures the time it takes to execute a block of code.
-
-    Usage:
-    with time_block() as elapsed_time:
-        # code block to be timed
-
-    Returns:
-    A timedelta object representing the elapsed time.
-    """
-    start = end = default_timer()
-    yield lambda: timedelta(seconds=end - start)
-    end = default_timer()
-
-def get_console_handler():
-   console_handler = logging.StreamHandler(sys.stdout)
-   console_handler.setFormatter(FORMATTER)
-   return console_handler
-def get_file_handler():
-   file_handler = TimedRotatingFileHandler(LOG_FILE, when='midnight')
-   file_handler.setFormatter(FORMATTER)
-   return file_handler
-def get_logger(logger_name):
-   logger = logging.getLogger(logger_name)
-   logger.setLevel(logging.DEBUG) # better to have too much log than not enough
-   logger.addHandler(get_console_handler())
-   logger.addHandler(get_file_handler())
-   # with this pattern, it's rarely necessary to propagate the error up to parent
-   logger.propagate = False
-   return logger
-
-LOGGER = get_logger('MassSeer')
-
-def check_im_array(im_array):
-    '''
-    Check Ion Mobility Array to make sure values are valid
-    '''
-    
-    if any(im_array<0):
-      raise click.ClickException(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] ERROR: There are values below 0 in the input ion mobility array! {im_array[im_array<0]}. Most likely a pyopenms memory view issue.")
-    elif 'e' in str(im_array):
-      raise click.ClickException(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] ERROR: The input ion mobility array seems to be in scientific notation! {im_array}. Most likely a pyopenms memory view issue.")
-
-def type_cast_value(value):
-    '''
-    Convert a string value to python literal
-    '''
-    if not isinstance(value, str):  # required for Click>=8.0.0
-        return value
-    try:
-        return ast.literal_eval(value)
-    except Exception:
-        raise click.BadParameter(value)
-
-def argument_value_log(args_dict):
-    '''
-    Print argument and value
-    '''
-    logging.debug("---------------- Input Parameters --------------------------")
-    for key, item in args_dict.items():
-        logging.debug(f"Parameter: {key} = {item}")
-    logging.debug("------------------------------------------------------------\n")
