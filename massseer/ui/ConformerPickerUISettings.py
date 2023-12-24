8755ad69a591cab42e9692ef41715c03cb36e4fe
singjc <justincsing@gmail.com>
2023-12-24 15:34:49 -0500

    [ADD] UI settings for conformer peak picking

--- massseer/ui/ConformerPickerUISettings.py
+++ massseer/ui/ConformerPickerUISettings.py
@@ -1,6 +1,7 @@
-import streamlit as st
 import os
+import streamlit as st
 
+# UI
 from massseer.ui.ChromatogramPlotUISettings import ChromatogramPlotUISettings
 
 DIRNAME = os.path.dirname(__file__)
@@ -18,6 +19,9 @@ class ConformerPickerUISettings:
         self.main_peak_picking_settings = main_peak_picking_settings
         self.shipped_model = True
         self.pretrained_model_file = None
+        self.conformer_window_size = 175
+        self.conformer_prediction_threshold = 0.5
+        self.conformer_prediction_type = "logits"
 
     def create_ui(self, plot_settings: ChromatogramPlotUISettings):
         """
@@ -30,7 +34,12 @@ class ConformerPickerUISettings:
         """
         self.shipped_model = st.sidebar.checkbox("Use shipped model", value=True, help="Use the shipped model.")
         if  self.shipped_model:
-            self.pretrained_model_file = os.path.join(DIRNAME, '..', 'models', 'conformer', 'base_cape.pt')
+            self.pretrained_model_file = os.path.join(DIRNAME, '..', 'models', 'conformer', 'base_cape.onnx')
             print(f"HERE: {self.pretrained_model_file}")
         else:
             self.pretrained_model_file = st.sidebar.text_input("Pretrained model file", value="", help="The pretrained model file to use.")
+        
+        with st.sidebar.expander("Advanced settings"):
+            self.conformer_window_size = st.number_input("window size", value=175, help="The window size for the conformer model, i.e the number of points of the chromatogram.")
+            self.conformer_prediction_threshold = st.number_input("prediction score threshold", value=0.2, help="The threshold for the conformer models prediction scores to find the top peak boundary.")
+            self.conformer_prediction_type = st.selectbox("prediction type", options=["logits", "sigmoided", "binarized"], help="The type of prediction to use for finding the top peak.")
