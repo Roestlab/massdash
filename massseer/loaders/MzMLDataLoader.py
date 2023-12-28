3b73a91631ed78116b73b910b74c74ff21526c15
singjc <justincsing@gmail.com>
2023-12-28 02:37:39 -0500

    [BUG] fix getting runname

--- massseer/loaders/MzMLDataLoader.py
+++ massseer/loaders/MzMLDataLoader.py
@@ -47,7 +47,7 @@ class MzMLDataLoader(GenericLoader):
         '''
         out = {}
         for t in self.dataFiles:
-            runname = basename(t).split('.')[0]
+            runname = basename(t.filename).split('.')[0]
             out[t] = self.rsltsFile.getTopTransitionGroupFeatureDf(runname, pep_id, charge)
         
         return pd.concat(out).reset_index().drop(columns='level_1').rename(columns=dict(level_0='filename'))
