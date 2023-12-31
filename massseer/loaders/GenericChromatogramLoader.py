e5d45d33ac2fc4a4f2c31395ce668a3a44e0c581
Joshua Charkow <joshuacharkow@gmail.com>
2023-12-31 17:18:53 -0500

    [FIX] GenericChromatogram + GenericSpectrum init

--- massseer/loaders/GenericChromatogramLoader.py
+++ massseer/loaders/GenericChromatogramLoader.py
@@ -7,4 +7,4 @@ from typing import List, Union, Literal
 
 class GenericChromatogramLoader(GenericLoader, ABC):
     def __init__(self, rsltsFile: str, dataFiles: Union[str, List[str]], libraryFile: str = None, rsltsFileType: Literal['OpenSWATH', 'DIA-NN'] = 'DIA-NN', verbose: bool=False, mode: Literal['module', 'gui'] = 'module'):
-        super().__init__()
+        super().__init__(rsltsFile=rsltsFile, dataFiles=dataFiles, libraryFile=libraryFile, rsltsFileType=rsltsFileType, verbose=verbose, mode=mode)
--- massseer/loaders/GenericSpectrumLoader.py
+++ massseer/loaders/GenericSpectrumLoader.py
@@ -8,7 +8,7 @@ from typing import List, Union, Literal
 
 class GenericSpectrumLoader(GenericLoader, ABC):
     def __init__(self, rsltsFile: str, dataFiles: Union[str, List[str]], libraryFile: str = None, rsltsFileType: Literal['OpenSWATH', 'DIA-NN'] = 'DIA-NN', verbose: bool=False, mode: Literal['module', 'gui'] = 'module'):
-        super().__init__()
+        super().__init__(rsltsFile=rsltsFile, dataFiles=dataFiles, libraryFile=libraryFile, rsltsFileType=rsltsFileType, verbose=verbose, mode=mode)
 
     @abstractmethod
     def loadFeatureMaps(self, pep_id: str, charge: int) -> dict[str, TransitionGroup]:
