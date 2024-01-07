8d13ff762c927c73b110cd8adfbb0e67bfaaa49e
singjc <justincsing@gmail.com>
2024-01-06 20:43:42 -0500

    [CAHNGE] test files

--- massseer/gui.py
+++ massseer/gui.py
@@ -90,18 +90,18 @@ def main(verbose, perf, perf_output):
 
     if st.session_state.workflow == "search_results_analysis" and st.session_state.clicked['load_toy_dataset_search_results_analysis']:
         # feature_file_path = os.path.join(dirname, '..', 'tests', 'test_data', 'osw', 'test_data.osw')
-        feature_file_path = "/media/justincsing/ExtraDrive1/Documents2/Roest_Lab/Github/MassSeer/tests/test_data/spyogenes/openswath/osw/merged.osw"
-        feature_file_type = "OpenSwath"
+        feature_file_path = os.path.join(dirname, '..', 'tests', 'test_data', 'example_dia', 'openswath', 'osw', 'test.osw')
+        feature_file_type = "OpenSWATH"
         
-        feature_file_path_2 = "/media/justincsing/ExtraDrive1/Documents2/Roest_Lab/Github/MassSeer/tests/test_data/spyogenes/diann/report/spyogenes_diann_two_runs.tsv"
+        feature_file_path_2 = os.path.join(dirname, '..', 'tests', 'test_data', 'example_dia', 'diann', 'report', 'test_combined_diann_report.tsv') 
         feature_file_type_2 = "DIA-NN"
         
-        feature_file_path_3 = "/media/justincsing/ExtraDrive1/Documents2/Roest_Lab/Github/MassSeer/tests/test_data/spyogenes/dreamdia/score_df_precursor.tsv"
+        feature_file_path_3 = os.path.join(dirname, '..', 'tests', 'test_data', 'example_dia', 'dreamdia', 'test_dreamdia_report.tsv') 
         feature_file_type_3 = "DreamDIA"
         
-        search_results_entries_dict = {'entry_1': {'search_results_file_path':          feature_file_path, 'search_results_exp_name': 'test_data', 'search_results_file_type': feature_file_type}, 
-                                        'entry_2': {'search_results_file_path': feature_file_path_2, 'search_results_exp_name': 'test_data_2', 'search_results_file_type': feature_file_type_2}, 
-                                        'entry_3': {'search_results_file_path': feature_file_path_3, 'search_results_exp_name': 'test_data_3', 'search_results_file_type': feature_file_type_3}}
+        search_results_entries_dict = {'entry_1': {'search_results_file_path':          feature_file_path, 'search_results_exp_name': 'OSW', 'search_results_file_type': feature_file_type}, 
+                                        'entry_2': {'search_results_file_path': feature_file_path_2, 'search_results_exp_name': 'DIA-NN', 'search_results_file_type': feature_file_type_2}, 
+                                        'entry_3': {'search_results_file_path': feature_file_path_3, 'search_results_exp_name': 'DreamDIA', 'search_results_file_type': feature_file_type_3}}
         
         massseer_gui.show_file_input_settings(feature_file_entries_dict=search_results_entries_dict)
         
