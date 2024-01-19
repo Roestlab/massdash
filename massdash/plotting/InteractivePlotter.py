aefb56c76a32262c15666968e6474d0c359e7e0b
Justin <justincsing@gmail.com>
2024-01-19 18:18:19 -0500

    [CLEAN] streamlit write message

--- massdash/plotting/InteractivePlotter.py
+++ massdash/plotting/InteractivePlotter.py
@@ -163,7 +163,7 @@ class InteractivePlotter(GenericPlotter):
             dark2_palette = Viridis256[0:len(features)]
 
         createBoundaryLegend = False
-        st.write('legend_labels', legend_labels)
+        
         if len(legend_labels) > 0:
             if len(legend_labels) != len(features):
                 raise ValueError("The number of legend labels must match the number of features")
