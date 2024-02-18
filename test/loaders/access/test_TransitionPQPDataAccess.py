404f52207bd0d9e71b63a50e6d28a112dd976aae
Joshua Charkow <joshuacharkow@gmail.com>
2024-02-17 20:09:29 -0500

    [TEST] update transitionPQP for syrupy

--- test/loaders/access/test_TransitionPQPDataAccess.py
+++ test/loaders/access/test_TransitionPQPDataAccess.py
@@ -1,35 +1,34 @@
-import unittest
 from massdash.loaders.access.TransitionPQPDataAccess import TransitionPQPDataAccess
-from snapshottest import TestCase
+import pytest
+from massdash.testing.PandasSnapshotExtension import PandasSnapshotExtenstion
 
-class TestTransitionPQPDataAccess(TestCase):
-    def setUp(self):
-        self.filename = "../../test_data/example_dia/openswath/lib/test.pqp"
-        self.data_access = TransitionPQPDataAccess(self.filename)
-    
-    def tearDown(self):
-        self.data_access.close()
-    
- 
-    def test_getTransitionList(self):
-        # test getTransitionList() method
-        transitions = self.data_access.getTransitionList()
-        self.assert_match_snapshot(transitions)
+@pytest.fixture
+def data_access():
+    filename = "../../test_data/example_dia/openswath/lib/test.pqp"
+    data_access = TransitionPQPDataAccess(filename)
+    yield data_access
+    data_access.close()
 
-    def test_validate_columns(self):
-        # test _validate_columns() method
-        self.data_access.data = self.data_access.getTransitionList()
-        self.assertTrue(self.data_access._validate_columns())
-    
-    def test_load(self):
-        # test load() method
-        self.data_access.load()
-        self.assertMatchSnapshot(self.data_access.data)
+@pytest.fixture
+def snapshot_pandas(snapshot):
+    return snapshot.use_extension(PandasSnapshotExtenstion)
 
-    def test_empty(self):
-        # test empty() method
-        self.data_access.load()
-        self.assertFalse(self.data_access.empty())
+def test_getTransitionList(data_access, snapshot_pandas):
+    # test getTransitionList() method
+    transitions = data_access.getTransitionList()
+    assert snapshot_pandas == transitions 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_validate_columns(data_access):
+    # test _validate_columns() method
+    data_access.data = data_access.getTransitionList()
+    assert data_access._validate_columns()
+
+def test_load(data_access, snapshot_pandas):
+    # test load() method
+    data_access.load()
+    assert snapshot_pandas == data_access.data 
+
+def test_empty(data_access):
+    # test empty() method
+    data_access.load()
+    assert not data_access.empty()
