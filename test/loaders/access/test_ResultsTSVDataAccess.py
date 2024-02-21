1a0e0fae6efb420544ad71a16c97e7750254af78
Joshua Charkow <joshuacharkow@gmail.com>
2024-02-21 11:08:22 -0500

    [TEST] update ResultsTSV access for new pytest format

--- test/loaders/access/test_ResultsTSVDataAccess.py
+++ test/loaders/access/test_ResultsTSVDataAccess.py
@@ -1,90 +1,56 @@
-import unittest
+import pytest
 from massdash.loaders.access.ResultsTSVDataAccess import ResultsTSVDataAccess
-from snapshottest import TestCase
+from massdash.testing.PandasSnapshotExtension import PandasSnapshotExtenstion
+from syrupy.extensions.amber import AmberDataSerializer
 
-class TestResultsTSVDataAccess(TestCase):
 
-    def setUp(self):
-        self.access_diann = ResultsTSVDataAccess("../../test_data/example_dia/diann/report/test_1_diann_report.tsv", 'DIA-NN')
-        #self.access_dream = ResultsTSVDataAccess("../../test_data/example_dia/dreamdia/test_dreamdia_report.tsv", 'DreamDIA')
+@pytest.fixture
+def access(request):
+    if request.param == 'diann':
+        return ResultsTSVDataAccess("../../test_data/example_dia/diann/report/test_1_diann_report.tsv", 'DIA-NN')
+    if request.param == 'dream':
+        return ResultsTSVDataAccess("../../test_data/example_dia/dreamdia/test_dreamdia_report.tsv", 'DreamDIA')
 
-        self.runname = 'test_raw_1'
-        self.peptide = 'DYASIDAAPEER'
-        self.charge = 2
+@pytest.fixture
+def runname():
+    return 'test_raw_1'
 
-    #### DIA-NN Access Tests ####
-    def test_loadData_diann(self):
-        # Test if the data is loaded correctly
-        data = self.access_diann.loadData()
-        self.assert_match_snapshot(data)
+@pytest.fixture
+def peptide():
+    return 'DYASIDAAPEER'
 
-    def test_initializePeptideHashTable_diann(self):
-        # Test if the peptide hash table is initialized correctly
-        hash_table = self.access_diann._initializePeptideHashTable()
-        self.assert_match_snapshot(hash_table)
+@pytest.fixture
+def charge():
+    return 2
 
-    def test_getTopTransitionGroupFeature_diann(self):
-        # Test if the top transition group feature is returned correctly
-        feature = self.access_diann.getTopTransitionGroupFeature(self.runname, self.peptide, self.charge)
-        self.assert_match_snapshot(feature)
-    
-    def test_getTransitionGroupFeatures_diann(self):
-        # Test for getTransitionGroupFeatures() with DIA-NN input
-        features = self.access_diann.getTransitionGroupFeatures(self.runname, self.peptide, self.charge)
-        self.assert_match_snapshot(features)
+@pytest.fixture
+def snapshot_pandas(snapshot):
+    return snapshot.use_extension(PandasSnapshotExtenstion)
 
-    def test_getTransitionGroupFeaturesDf_diann(self):
-        # Test for getTransitionGroupFeaturesDf() with DIA-NN input
-        df = self.access_diann.getTransitionGroupFeaturesDf(self.runname, self.peptide, self.charge)
-        self.assert_match_snapshot(df)
+@pytest.mark.parametrize("access", ['diann', 'dream'], indirect=True)
+def test_loadData(access, snapshot_pandas):
+    data = access.loadData()
+    assert snapshot_pandas == data
 
-    def test_getTopTransitionGroupFeatureDf_diann(self):
-        # Test for getTopTransitionGroupFeatureDf() with DIA-NN input
-        df = self.access_diann.getTopTransitionGroupFeatureDf(self.runname, self.peptide, self.charge)
-        self.assert_match_snapshot(df)
-    
+@pytest.mark.parametrize("access", ['diann', 'dream'], indirect=True)
+def test_initializePeptideHashTable(access, snapshot_pandas):
+    hash_table = access._initializePeptideHashTable()
+    assert snapshot_pandas == hash_table
 
-    def test_getExactRunName(self):
-        # Test if the exact run name is returned correctly only tested for DIA-NN currently
-        run_basename_wo_ext = "test_raw_1"
-        exact_run_name = self.access_diann.getExactRunName(run_basename_wo_ext)
-        self.assertIsInstance(exact_run_name, str)
-        self.assertEqual(exact_run_name, "test_raw_1")
-    
-    
-    
-    ### DreamDIA Access Tests ###
-    '''
-    def test_loadData_dream(self):
-        # Test if the data is loaded correctly
-        data = self.access_dream.loadData()
-        self.assert_match_snapshot(data)
+#@pytest.mark.parametrize("access,runname,peptide,charge", [('diann', 'test_raw_1', 'DYASIDAAPEER', 2), ('dream', 'test_raw_1', 'DYASIDAAPEER', 2)], indirect=['access'])
+@pytest.mark.parametrize("access,runname,peptide,charge", [('diann', 'test_raw_1', 'DYASIDAAPEER', 2),], indirect=['access'])
+def test_getTopTransitionGroupFeature(access, runname, peptide, charge, snapshot):
+    feature = access.getTopTransitionGroupFeature(runname, peptide, charge)
+    assert snapshot == AmberDataSerializer.object_as_named_tuple(feature)
 
-    def test_initializePeptideHashTable_dream(self):
-        # Test if the peptide hash table is initialized correctly
-        hash_table = self.access_dream._initializePeptideHashTable()
-        self.assert_match_snapshot(hash_table)
+#@pytest.mark.parametrize("access,runname,peptide,charge", [('diann', 'test_raw_1', 'DYASIDAAPEER', 2), ('dream', 'test_raw_1', 'DYASIDAAPEER', 2)], indirect=['access'])
+@pytest.mark.parametrize("access,runname,peptide,charge", [('diann', 'test_raw_1', 'DYASIDAAPEER', 2),], indirect=['access'])
+def test_getTransitionGroupFeatures(access, runname, peptide, charge, snapshot):
+    features = access.getTransitionGroupFeatures(runname, peptide, charge)
+    assert snapshot == [ AmberDataSerializer.object_as_named_tuple(i) for i in features ]
 
-    def test_getTransitionGroupFeatures_dream(self):
-        # Test for getTransitionGroupFeatures() with dreamDIA input
-        features = self.access_dream.getTransitionGroupFeatures(self.runname, self.peptide, self.charge)
-        self.assert_match_snapshot(features)
-
-    def test_getTopTransitionGroupFeature_dream(self):
-        # Test for getTopTransitionGroupFeature() with dreamDIA input
-        feature = self.access_dream.getTopTransitionGroupFeature(self.runname, self.peptide, self.charge)
-        self.assert_match_snapshot(feature)
-
-    def test_getTransitionGroupFeaturesDf_dream(self):
-        # Test for getTransitionGroupFeaturesDf() with dreamDIA input
-        df = self.access_dream.getTransitionGroupFeaturesDf(self.runname, self.peptide, self.charge)
-        self.assert_match_snapshot(df)
-
-    def test_getTopTransitionGroupFeatureDf_dream(self):
-        # Test for getTopTransitionGroupFeatureDf() with dreamDIA input
-        df = self.access_dream.getTopTransitionGroupFeatureDf(self.runname, self.peptide, self.charge)
-        self.assert_match_snapshot(df)
-    '''
-
-if __name__ == "__main__":
-    unittest.main()
+#@pytest.mark.parametrize("access,runname,peptide,charge", [('diann', 'test_raw_1', 'DYASIDAAPEER', 2), ('dream', 'test_raw_1', 'DYASIDAAPEER', 2)], indirect=['access'])
+@pytest.mark.parametrize("access,runname,peptide,charge", [('diann', 'test_raw_1', 'DYASIDAAPEER', 2),], indirect=['access'])
+def test_getTransitionGroupFeaturesDf(access, runname, peptide, charge, snapshot_pandas):
+    df = access.getTransitionGroupFeaturesDf(runname, peptide, charge)
+    assert snapshot_pandas == df
