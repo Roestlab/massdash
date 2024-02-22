82aa29f6604b6f9ef89dabc440f29a4128a82c67
Joshua Charkow <joshuacharkow@gmail.com>
2024-02-22 08:31:37 -0500

    [TEST] TransitionTSVDataAccess with pytest+pandas

--- test/loaders/access/test_TransitionTSVDataAccess.py
+++ test/loaders/access/test_TransitionTSVDataAccess.py
@@ -1,48 +1,42 @@
-import unittest
+import pytest
 from massdash.loaders.access.TransitionTSVDataAccess import TransitionTSVDataAccess
-from snapshottest import TestCase
+from massdash.testing.PandasSnapshotExtension import PandasSnapshotExtenstion
 import pandas as pd
 
-class TestTransitionTSVDataAccess(TestCase):
+@pytest.fixture
+def filename():
+    return "../../test_data/example_dia/diann/lib/test_1_lib.tsv"
 
-    def setUp(self):
-        self.filename = "../../test_data/example_dia/diann/lib/test_1_lib.tsv"
-        self.data_access = TransitionTSVDataAccess(self.filename)
+@pytest.fixture
+def snapshot_pandas(snapshot):
+    return snapshot.use_extension(PandasSnapshotExtenstion)
 
-    def test_resolve_column_names(self):
-        # Test for _resolve_column_names() method
-        data_access = TransitionTSVDataAccess(self.filename)
-        data_access.data = pd.read_csv(self.filename, sep='\t')
-        data_access._resolve_column_names()
-        self.assert_match_snapshot(data_access.data)
-     
-    def test_generate_annotation(self):
-        # Test for _generate_annotation() method
-        data_access = TransitionTSVDataAccess(self.filename)
-        data_access.data = pd.read_csv(self.filename, sep='\t')
-        data_access._resolve_column_names()
-        data_access.generate_annotation()
-        self.assert_match_snapshot(data_access.data['Annotation'])
+@pytest.fixture
+def data_access():
+    return TransitionTSVDataAccess("../../test_data/example_dia/diann/lib/test_1_lib.tsv")
 
-    def test_validate_columns(self):
-        # Test for _validate_columns() method
-        data_access = TransitionTSVDataAccess(self.filename)
-        data_access.data = pd.read_csv(self.filename, sep='\t')
-        data_access._resolve_column_names()
-        data_access.generate_annotation()
-        data_access._validate_columns()
-        #missing_columns = set(TransitionTSVDataAccess.REQUIRED_TSV_COLUMNS).difference(set(data_access.data.columns))
-        self.assertTrue(data_access._validate_columns())
-   
-    def test_load(self):
-        self.data_access.load()
-        self.assertMatchSnapshot(self.data_access.data)
-   
-    def test_empty(self):
-        # Test if the data is empty
-        self.data_access.load()
-        self.assertFalse(self.data_access.empty())
+def test_resolve_column_names(data_access, snapshot_pandas):
+    data_access.data = pd.read_csv(data_access.filename, sep='\t')
+    data_access._resolve_column_names()
+    assert snapshot_pandas == data_access.data
 
+def test_generate_annotation(data_access, snapshot):
+    data_access.data = pd.read_csv(data_access.filename, sep='\t')
+    data_access._resolve_column_names()
+    data_access.generate_annotation()
+    assert snapshot_pandas == data_access.data
 
-if __name__ == '__main__':
-    unittest.main()
+def test_validate_columns(data_access):
+    data_access.data = pd.read_csv(data_access.filename, sep='\t')
+    data_access._resolve_column_names()
+    data_access.generate_annotation()
+    data_access._validate_columns()
+    assert data_access._validate_columns()
+
+def test_load(data_access, snapshot_pandas):
+    data_access.load()
+    assert snapshot_pandas == data_access.data
+
+def test_empty(data_access):
+    data_access.load()
+    assert not data_access.empty()
