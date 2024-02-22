f73c1daec9a86fc3c997ad3a705a608a0c231c9f
Joshua Charkow <joshuacharkow@gmail.com>
2024-02-22 09:57:52 -0500

    [TEST] sqMassDataAccess update to snapshots/pytest

--- test/loaders/access/test_SqMassDataAccess.py
+++ test/loaders/access/test_SqMassDataAccess.py
@@ -3,86 +3,71 @@ test/loaders/test_SqMassDataAccess
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
 
-import unittest
+import pytest
 from pathlib import Path
-
 import pandas as pd
-from snapshottest import TestCase
 
 from massdash.loaders.access.SqMassDataAccess import SqMassDataAccess
-from massdash.structs.Chromatogram import Chromatogram
+from massdash.testing.PandasSnapshotExtension import PandasSnapshotExtenstion
+from syrupy.extensions.amber import AmberDataSerializer
 from massdash.util import find_git_directory
 
 TEST_PATH = find_git_directory(Path(__file__).resolve()).parent / 'test'
 
+@pytest.fixture
+def mass_data_access():
+    db_path = f"{str(TEST_PATH)}/test_data/xics/test_chrom_1.sqMass"
+    mass_data_access = SqMassDataAccess(db_path)
+    yield mass_data_access
+    mass_data_access.conn.close()
 
-class TestSqMassDataAccess(TestCase):
-    def setUp(self):
-        self.db_path = f"{str(TEST_PATH)}/test_data/xics/test_chrom_1.sqMass"
-        self.mass_data_access = SqMassDataAccess(self.db_path)
-
-    def test_getPrecursorChromIDs(self):
-        precursor_id = 30
-        chrom_ids = self.mass_data_access.getPrecursorChromIDs(precursor_id)["chrom_ids"]
-        self.assertMatchSnapshot(chrom_ids)
-
-    def test_getDataForChromatograms(self):
-        ids = [41353, 41354, 41387]
-        data = self.mass_data_access.getDataForChromatograms(ids, ["y4^1", "y3^1", "y6^1"])
-        self.assertMatchSnapshot(len(data))
-        self.assertIsInstance(data[0], Chromatogram)
-        self.assertIsInstance(data[1], Chromatogram)
-        self.assertIsInstance(data[2], Chromatogram)
-
-    def test_getDataForChromatogram(self):
-        myid = 424
-        data = self.mass_data_access.getDataForChromatogram(myid, "y4^1")
-        self.assertIsInstance(data, Chromatogram)
-
-    def test_getDataForChromatogramFromNativeId(self):
-        native_id = 992928
-        data = self.mass_data_access.getDataForChromatogramFromNativeId(native_id)
-        self.assertIsInstance(data[0], list)
-        self.assertIsInstance(data[1], list)
-
-    def test_getDataForChromatogramsFromNativeIds(self):
-        native_ids = [180, 181, 182, 183, 183, 185]
-        labels = ["y4^1", "y3^1", "y6^1", "y7^1", "y7^1", "y8^1"]
-        data = self.mass_data_access.getDataForChromatogramsFromNativeIds(native_ids, labels)
-        self.assertMatchSnapshot(len(data))
-        self.assertIsInstance(data[0], Chromatogram)
-        self.assertIsInstance(data[1], Chromatogram)
-        self.assertIsInstance(data[2], Chromatogram)
-        self.assertMatchSnapshot(data)
+@pytest.fixture
+def snapshot_pandas(snapshot):
+    return snapshot.use_extension(PandasSnapshotExtenstion)
 
 
-    def test_getDataForChromatogramsDf(self):
-        ids = [41353, 41354, 41387]
-        data = self.mass_data_access.getDataForChromatogramsDf(ids, ["y4^1", "y3^1", "y6^1"])
-        self.assertIsInstance(data, pd.DataFrame)
-        self.assertMatchSnapshot(data)
+def test_getPrecursorChromIDs(mass_data_access, snapshot):
+    precursor_id = 30
+    chrom_ids = mass_data_access.getPrecursorChromIDs(precursor_id)
+    assert snapshot == chrom_ids
 
-        ## test invalid ids
-        ids = [0]
-        data = self.mass_data_access.getDataForChromatogramsDf(ids, ["y4^1"])
-        self.assertIsInstance(data, pd.DataFrame)
-        self.assert_match_snapshot(data)
+def test_getDataForChromatograms(mass_data_access, snapshot_pandas):
+    ids = [41353, 41354, 41387]
+    annots = ["y4^1", "y3^1", "y6^1"]
+    data = mass_data_access.getDataForChromatograms(ids, annots)
+    assert len(data) == 3
 
-        ## test empty ids
-        ids = []
-        data = self.mass_data_access.getDataForChromatogramsDf(ids, ["y4^1"])
-        self.assertIsInstance(data, pd.DataFrame)
-        self.assert_match_snapshot(data)
+    # assemble the data into snapshot
+    data_out = pd.concat([d.toPandasDf() for d in data]) 
+    assert snapshot_pandas == data_out
 
-    def test_getDataForChromatogramsFromNativeIdsDf(self):
-        ids = [180, 181, 182, 183, 183, 185]
-        labels = ["y4^1", "y3^1", "y6^1", "y7^1", "y7^1", "y8^1"]
-        data = self.mass_data_access.getDataForChromatogramsFromNativeIdsDf(ids, labels)
-        self.assertIsInstance(data, pd.DataFrame)
-        self.assert_match_snapshot(data)
+def test_getDataForChromatogram(mass_data_access, snapshot_pandas):
+    myid = 424
+    data = mass_data_access.getDataForChromatogram(myid, "y4^1")
+    data_out = data.toPandasDf()
+    assert snapshot_pandas == data_out
 
-    def tearDown(self):
-        self.mass_data_access.conn.close()
+def test_getDataForChromatogramFromNativeId(mass_data_access, snapshot_pandas):
+    native_id = 992928
+    data = mass_data_access.getDataForChromatogramFromNativeId(native_id)
+    data_out = pd.DataFrame(data)
+    assert snapshot_pandas == data_out
 
-if __name__ == '__main__':
-    unittest.main()
+def test_getDataForChromatogramsFromNativeIds(mass_data_access, snapshot_pandas):
+    native_ids = [180, 181, 182, 183, 183, 185]
+    labels = ["y4^1", "y3^1", "y6^1", "y7^1", "y7^1", "y8^1"]
+    data = mass_data_access.getDataForChromatogramsFromNativeIds(native_ids, labels)
+    data_out = pd.concat([d.toPandasDf() for d in data])
+    assert snapshot_pandas == data_out
+
+@pytest.mark.parametrize("ids,labels", [([41353, 41354, 41387], ["y4^1", "y3^1", "y6^1"]), ([0], ["y4^1"]), ([], ["y4^1"])])
+def test_getDataForChromatogramsDf(mass_data_access, snapshot_pandas, ids, labels):
+    # Assuming you have some data to test this method
+    df = mass_data_access.getDataForChromatogramsDf(ids, labels)
+    assert snapshot_pandas == df
+
+def test_getDataForChromatogramsFromNativeIdsDf(mass_data_access, snapshot_pandas):
+    ids = [180, 181, 182, 183, 183, 185]
+    labels = ["y4^1", "y3^1", "y6^1", "y7^1", "y7^1", "y8^1"]
+    data = mass_data_access.getDataForChromatogramsFromNativeIdsDf(ids, labels)
+    assert snapshot_pandas == data
