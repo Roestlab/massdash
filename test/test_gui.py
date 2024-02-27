5a566bd3e3f6bb1223279485faae6353c06c9a33
Joshua Charkow <joshuacharkow@gmail.com>
2024-02-27 13:48:53 -0500

    [TEST] add gui test for test examples
    basic examples where load the example data
    
    Problem: currently does not test all of the different settings

--- test/test_gui.py
+++ test/test_gui.py
@@ -7,7 +7,10 @@ GUI_DIR = str(find_git_directory(Path(__file__).resolve()).parent / 'massdash' /
 
 @pytest.fixture
 def at():
-    return AppTest(GUI_DIR, default_timeout=60)
+    at =  AppTest(GUI_DIR, default_timeout=60)
+    at.session_state.clicked  = {'load_toy_dataset_xic_data':False, 'load_toy_dataset_raw_data':False, 'load_toy_dataset_search_results_analysis':False}
+    at.session_state.workflow = None
+    return at
 
 def test_welcome_screen(at):
     at.run()
@@ -15,13 +18,13 @@ def test_welcome_screen(at):
 
 def test_extracted_data_example(at):
     at.session_state.workflow = 'xic_data'
-    at.click("Load OpenSwath Example")
+    at.session_state.clicked['load_toy_dataset_xic_data'] = True
     at.run()
     assert not at.exception
-'''
+
 def test_raw_data_example(at):
     at.session_state.workflow = 'raw_data'
-    at.session_state.clicked['load_toy_dataset_raw_data_im'] = True
+    at.session_state.clicked['load_toy_dataset_raw_data'] = True
     at.run()
     assert not at.exception
 
@@ -31,6 +34,8 @@ def test_raw_data_im_example(at):
     at.run()
     assert not at.exception
 
+# Fails currently 
+'''
 def test_search_results_analysis_example(at):
     at.session_state.workflow = 'search_results_analysis'
     at.session_state.clicked['load_toy_dataset_search_results_analysis'] = True
