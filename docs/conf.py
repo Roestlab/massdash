71b20bd25416256fd6ddb3e51ff2c0953c4c92e0
Justin <justincsing@gmail.com>
2024-03-02 00:52:50 -0500

    add: dependency for markup support

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ author = 'RoestLab'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.napoleon', 'sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'nbsphinx', 'sphinx.ext.autosectionlabel', 'sphinx_copybutton' ]
+extensions = ['sphinx.ext.napoleon', 'sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'nbsphinx', 'sphinx.ext.autosectionlabel', 'sphinx_copybutton', 'myst_parser' ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
