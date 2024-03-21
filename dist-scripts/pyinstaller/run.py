913c024febf51bd09b34f2c3f1c06d61fcda4833
Justin Sing <32938975+singjc@users.noreply.github.com>
2024-03-21 12:57:14 -0700

    Update run.py

--- dist-scripts/pyinstaller/run.py
+++ dist-scripts/pyinstaller/run.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
         from massdash.util import get_free_port
         if sys.platform[:6] == "darwin":
 	        os.environ['KMP_DUPLICATE_LIB_OK']='True'
-        use_port = "8509" # str(get_free_port())
+        use_port = str(get_free_port())
         massdash.main.gui(['--verbose', '--server_port', use_port, '--no_global_developmentMode', '--no_browser_gatherUsageStats'], standalone_mode=False)
     except ImportError:
         import sys
