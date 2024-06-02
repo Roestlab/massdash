bb9715e4c94f42899df59acf91c308a62952f750
singjc <justincsing@gmail.com>
2024-06-02 01:48:10 -0400

    refactor: plotting for generalized scatter plot based on df

--- massdash/plotting/DebugPlotter.py
+++ massdash/plotting/DebugPlotter.py
@@ -16,10 +16,10 @@ class DebugPlotter:
     def __init__(self):
         self.fig = None
         
-    def plot(self, df):
+    def plot(self, df, x_col, y_col, title, x_axis_label, y_axis_label):
         # Create a new plot
-        p = figure(title='Retention time transformation', x_axis_label='original RT [s]', y_axis_label='Delta RT [s]',
-           tools=['pan', 'wheel_zoom', 'box_zoom', 'reset', 'save'])
+        p = figure(title=title, x_axis_label=x_axis_label, y_axis_label=y_axis_label,
+                tools=['pan', 'wheel_zoom', 'box_zoom', 'reset', 'save'])
 
         unique_filenames = df['filename'].unique()
         colors = Category20[len(unique_filenames)]
@@ -27,19 +27,18 @@ class DebugPlotter:
         legend_it = []
         file_number = 1
         for filename, grouped_df in df.groupby('filename'):
-            
-            color = colors[file_number-1]
-            print(f"File {file_number}: {filename} | color: {color}")
+            color = colors[file_number - 1]
+            print(f"File {file_number}: {filename}")
             # Add the scatter plot
             source = ColumnDataSource(grouped_df)
-            renderer = p.scatter('experiment_rt', 'library_rt', source=source, size=10, alpha=0.5, color=color)
+            renderer = p.scatter(x_col, y_col, source=source, size=10, alpha=0.5, color=color)
             legend_it.append((f"File {file_number}", [renderer]))
             file_number += 1
 
         # Configure the minimal hover tool
         hover_minimal = HoverTool(tooltips=[
-            ('original RT', '@experiment_rt{0.0}'),
-            ('Delta RT', '@library_rt{0.0}'),
+            (x_axis_label, f'@{x_col}{{0.0}}'),
+            (y_axis_label, f'@{y_col}{{0.0}}'),
             ('Peptide Sequence', '@ModifiedPeptideSequence')
         ], name="Minimal Hover")
         p.add_tools(hover_minimal)
@@ -57,7 +56,7 @@ class DebugPlotter:
 
         # Add a legend for the filename
         legend = Legend(items=legend_it)
-        legend.click_policy="mute"
+        legend.click_policy = "mute"
         legend.label_text_font_size = '8pt'
         p.add_layout(legend, 'right')
 
