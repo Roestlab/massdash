68a55e61d13323dd522b2c433b0952fe1b5d8230
Joshua Charkow <joshuacharkow@gmail.com>
2024-10-08 14:07:37 -0400

    test: add multi-result file tests for sqmassLoader
    multi result file tests for sqMassLoader

--- test/loaders/test_SqMassLoader.py
+++ test/loaders/test_SqMassLoader.py
@@ -11,40 +11,46 @@ from massdash.loaders import SqMassLoader
 from massdash.util import find_git_directory
 from massdash.testing import PandasSnapshotExtension
 
-TEST_PATH = find_git_directory(Path(__file__).resolve()).parent / 'test'
+TEST_PATH = find_git_directory(Path(__file__).resolve()).parent / 'test' / 'test_data' / 'example_dia'
 
-@pytest.fixture
-def loader():
-    return SqMassLoader(dataFiles=[f'{str(TEST_PATH)}/test_data/xics/test_chrom_1.sqMass', f'{str(TEST_PATH)}/test_data/xics/test_chrom_2.sqMass'], rsltsFile=f"{str(TEST_PATH)}/test_data/osw/test_data.osw")
+@pytest.fixture(params=['openswath', 'combined'])
+def loader(request):
+    dataFiles = ['test_raw_1.sqMass', 'test_raw_2.sqMass']
+    dataFiles = [ str(TEST_PATH / 'openswath' / 'xics' / f) for f in dataFiles ]
+
+    if request.param == 'openswath':
+        return SqMassLoader(dataFiles=dataFiles, rsltsFile=str(TEST_PATH / 'openswath' / 'osw' / "test.osw"))
+    if request.param == 'combined':
+        return SqMassLoader(dataFiles=dataFiles, 
+                            rsltsFile=[str(TEST_PATH / 'openswath' / 'osw' / "test.osw"), 
+                                       str(TEST_PATH / 'diann' / 'report' / "test_diann_report_combined.tsv")])
 
 @pytest.fixture
 def snapshot_pandas(snapshot):
     return snapshot.use_extension(PandasSnapshotExtension)
 
-'''
-@pytest.mark.parametrize('fullpeptidename,charge', [('NKESPT(UniMod:21)KAIVR(UniMod:267)', 3), ('INVALID', 0)])
+@pytest.mark.parametrize('fullpeptidename,charge', [('AGAANIVPNSTGAAK', 3), ('INVALID', 0)])
 def test_loadTransitionGroupFeature(loader, fullpeptidename, charge, snapshot):
     # Test loading a peak feature for a valid peptide ID and charge
     peak_feature = loader.loadTransitionGroupFeatures(fullpeptidename, charge)
     assert snapshot == AmberDataSerializer.serialize(peak_feature)
-'''
 
-@pytest.mark.parametrize('fullpeptidename,charge', [('NKESPT(UniMod:21)KAIVR(UniMod:267)', 3), ('INVALID', 0)])
+@pytest.mark.parametrize('fullpeptidename,charge', [('AGAANIVPNSTGAAK', 3), ('INVALID', 0)])
 def test_loadTransitionGroups(loader, fullpeptidename, charge, snapshot):
     # Test loading a chromatogram for a valid peptide ID and charge
     transitionGroup = loader.loadTransitionGroups(fullpeptidename, charge) 
+    print(transitionGroup)
     assert snapshot == AmberDataSerializer.serialize(transitionGroup)
 
-@pytest.mark.parametrize('fullpeptidename,charge', [('NKESPT(UniMod:21)KAIVR(UniMod:267)', 3), ('INVALID', 0)])
+@pytest.mark.parametrize('fullpeptidename,charge', [('AGAANIVPNSTGAAK', 3), ('INVALID', 0)])
 def test_loadTransitionGroupsDf(loader, fullpeptidename, charge, snapshot_pandas):
     # Test loading a chromatogram for a valid peptide ID and charge
     transitionGroup = loader.loadTransitionGroupsDf(fullpeptidename, charge) 
+    print(transitionGroup)
     assert snapshot_pandas == transitionGroup 
 
-'''
-@pytest.mark.parametrize('fullpeptidename,charge', [('NKESPT(UniMod:21)KAIVR(UniMod:267)', 3), ('INVALID', 0)])
+@pytest.mark.parametrize('fullpeptidename,charge', [('AGAANIVPNSTGAAK', 3), ('INVALID', 0)])
 def test_loadTransitionGroupFeaturesDf(loader, fullpeptidename, charge, snapshot_pandas):
     # Test loading a chromatogram for a valid peptide ID and charge
     transitionGroup = loader.loadTransitionGroupFeaturesDf(fullpeptidename, charge)
     assert snapshot_pandas == transitionGroup
-'''
