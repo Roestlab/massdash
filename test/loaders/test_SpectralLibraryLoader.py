cd86f0fdc10e955fdd3aaede9bb7dfc771cefb5b
Joshua Charkow <joshuacharkow@gmail.com>
2025-08-18 20:29:39 -0400

    test: cast np.float to float

--- test/loaders/test_SpectralLibraryLoader.py
+++ test/loaders/test_SpectralLibraryLoader.py
@@ -61,7 +61,7 @@ def test_get_unique_charge_states_per_peptide(spectral_library_loader, snapshot)
 
 @pytest.mark.parametrize('spectral_library_loader,pep,charge', [('diann','DYASIDAAPEER', 2), ('openswath', 'DYASIDAAPEER', 2)], indirect=['spectral_library_loader'])
 def test_get_peptide_precursor_mz(spectral_library_loader, pep, charge, snapshot):
-    precursor_mz = spectral_library_loader.get_peptide_precursor_mz(pep, charge)
+    precursor_mz = float(spectral_library_loader.get_peptide_precursor_mz(pep, charge))
     assert snapshot == precursor_mz
 
 
@@ -87,19 +87,19 @@ def test_get_peptide_product_charge_list(spectral_library_loader, pep, charge, s
 # note: does not work with DIA-NN since no NormalizedRetentionTime column
 @pytest.mark.parametrize('spectral_library_loader,pep,charge', [('openswath', 'DYASIDAAPEER', 2)], indirect=['spectral_library_loader'])
 def test_get_peptide_retention_time(spectral_library_loader, pep, charge, snapshot):
-    retention_time = spectral_library_loader.get_peptide_retention_time(pep, charge)
+    retention_time = float(spectral_library_loader.get_peptide_retention_time(pep, charge))
     assert snapshot == retention_time
 
 
 @pytest.mark.parametrize('spectral_library_loader,pep,charge', [('diann-im','AFVDFLSDEIK', 2), ], indirect=['spectral_library_loader'])
 def test_get_peptide_ion_mobility(spectral_library_loader, pep, charge, snapshot):
-    ion_mobility = spectral_library_loader.get_peptide_ion_mobility(pep, charge)
+    ion_mobility = float(spectral_library_loader.get_peptide_ion_mobility(pep, charge))
     assert snapshot == ion_mobility
 
 
 @pytest.mark.parametrize('spectral_library_loader,pep,charge', [('diann','DYASIDAAPEER', 2), ('openswath', 'DYASIDAAPEER', 2)], indirect=['spectral_library_loader'])
 def test_get_peptide_library_intensity(spectral_library_loader, pep, charge, snapshot):
-    library_intensity = spectral_library_loader.get_peptide_library_intensity(pep, charge)
+    library_intensity = float(spectral_library_loader.get_peptide_library_intensity(pep, charge))
     assert snapshot == library_intensity
 
 
@@ -111,7 +111,7 @@ def test_get_peptide_fragment_annotation_list(spectral_library_loader, pep, char
 
 @pytest.mark.parametrize('spectral_library_loader,pep,charge,fragment', [('diann','DYASIDAAPEER', 2, 'b3^1'), ('openswath', 'DYASIDAAPEER', 2, 'y4/0.01y10^2/0.50')], indirect=['spectral_library_loader'])
 def test_get_fragment_library_intensity(spectral_library_loader, pep, charge, fragment, snapshot):
-    library_intensity = spectral_library_loader.get_fragment_library_intensity(pep, charge, fragment)
+    library_intensity = float(spectral_library_loader.get_fragment_library_intensity(pep, charge, fragment))
     assert snapshot == library_intensity
 
 @pytest.mark.parametrize('spectral_library_loader,pep,charge', [('diann','DYASIDAAPEER', 2), ('openswath', 'DYASIDAAPEER', 2)], indirect=['spectral_library_loader'])
